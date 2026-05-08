6a83e813630334c145bb4eaddbd51ff9f8f53bf7
clofour <208706398+clofour@users.noreply.github.com>
2026-05-08 21:23:02 +0200

    Add fields for events

--- frontend/src/components/calendar/EventForm.tsx
+++ frontend/src/components/calendar/EventForm.tsx
@@ -1,15 +1,16 @@
-import { Button, Group, NumberInput, Stack, Textarea, TextInput } from "@mantine/core";
+import { Button, Checkbox, Combobox, Group, Input, InputBase, NumberInput, Stack, Tabs, Textarea, TextInput, useCombobox } from "@mantine/core";
 import { useForm, schemaResolver } from "@mantine/form";
 import { postApiGoalCreateBearing } from "@/api/endpoints/goal/goal.js";
 import { PostApiGoalCreateBearingBody } from "@/api/endpoints/goal/goal.zod.js";
 import { getErrorMessage } from "@/data/error";
+import { useState } from "react";
 
 interface EventFormProps {
     close: () => void;
     setAlert: (alert: string) => void;
 }
 
-export default function EventForm({close, setAlert}: EventFormProps) {
+export default function EventForm({ close, setAlert }: EventFormProps) {
     const formSchema = PostApiGoalCreateBearingBody.omit({
         northStarId: true
     })
@@ -17,7 +18,6 @@ export default function EventForm({close, setAlert}: EventFormProps) {
         mode: 'uncontrolled',
         validate: schemaResolver(formSchema, { sync: true })
     })
-
     const handleSubmit = async (values: typeof form.values) => {
         const requestData = {
             ...values
@@ -31,30 +31,116 @@ export default function EventForm({close, setAlert}: EventFormProps) {
         }
     };
 
+    const unitCombobox = useCombobox({
+        onDropdownClose: () => unitCombobox.resetSelectedOption()
+    })
+    const [unit, setUnit] = useState<string | null>(null);
+    const unitOptions = ["day", "week", "month", "year"].map((option) => (
+        <Combobox.Option value={option} key={option}>
+            {option}
+        </Combobox.Option>
+    ))
+
     return (
         <>
             <form onSubmit={form.onSubmit(handleSubmit)}>
                 <Stack>
-                    <NumberInput
-                        label="Amount"
-                        description="What is your goal?"
+                    <TextInput
+                        label="Name"
+                        description="What should this event be called?"
                         placeholder="1"
                         required
-                        key={form.key('amount')}
-                        {...form.getInputProps('amount')}
+                        key={form.key('name')}
+                        {...form.getInputProps('name')}
                     />
 
-                    <NumberInput
-                        label="Amount"
-                        description="What is your goal?"
-                        placeholder="1"
-                        required
-                        key={form.key('amount')}
-                        {...form.getInputProps('amount')}
-                    />
-                    
 
-                    <Group justify="flex-end" mt="md">
+                    <Group grow justify="flex-between">
+                        <NumberInput
+                            label="Amount"
+                            description="What should this event be called?"
+                            placeholder="1"
+                            required
+                            key={form.key('amount')}
+                            {...form.getInputProps('amount')}
+                        />
+                        <Combobox
+                            store={unitCombobox}
+                            onOptionSubmit={(value) => {
+                                setUnit(value);
+                                unitCombobox.closeDropdown();
+                            }}
+                            key={form.key('unit')}
+                            {...form.getInputProps('unit')}
+                        >
+                            <Combobox.Target>
+                                <InputBase
+                                    label="Unit"
+                                    description="What should this event be called?"
+                                    required
+                                    component="button"
+                                    type="button"
+                                    pointer
+                                    rightSection={<Combobox.Chevron />}
+                                    rightSectionPointerEvents="none"
+                                    onClick={() => unitCombobox.toggleDropdown()}
+                                >
+                                    {unit || <Input.Placeholder>Pick unit</Input.Placeholder>}
+                                </InputBase>
+                            </Combobox.Target>
+
+                            <Combobox.Dropdown>
+                                <Combobox.Options>{unitOptions}</Combobox.Options>
+                            </Combobox.Dropdown>
+                        </Combobox>
+                    </Group>
+
+                    {unit == "week" && (
+                        <Checkbox.Group
+                            label="Day of the week"
+                            description="Which day of the week should this event take place?"
+                            required
+                            key={form.key('weekday')}
+                            {...form.getInputProps('weekday')}
+                        >
+                            <Group mt="xs">
+                                {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map((weekday) => (
+                                    <Checkbox key={weekday} value={weekday} label={weekday} />
+                                ))}
+                            </Group>
+                        </Checkbox.Group>
+                    )}
+
+                    {unit == "month" && (
+                        <NumberInput
+                            label="Day of the month"
+                            description="Which day of the month should this event take place?"
+                            required
+                            key={form.key('monthday')}
+                            {...form.getInputProps('monthday')}
+                        />
+                    )}
+
+                    {unit == "year" && (
+                        <Group grow justify="flex-between">
+                            <NumberInput
+                                label="Day of the month"
+                                description="Which day of the month should this event take place?"
+                                required
+                                key={form.key('monthday')}
+                                {...form.getInputProps('monthday')}
+                            />
+                            <NumberInput
+                                label="Month of the year"
+                                description="Which month of the eyar should this event take place?"
+                                required
+                                key={form.key('yearmonth')}
+                                {...form.getInputProps('yearmonth')}
+                            />
+                        </Group>
+                    )}
+
+                    <Group justify="flex-end">
                         <Button type="submit">Submit</Button>
                     </Group>
                 </Stack>
